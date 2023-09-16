e9547d73e4448ba267ad69837c41b618cdae03b7
genshuuriki <saveliyivlev10@gmail.com>
2023-09-16 16:41:36 +0300

    Lab

---
+++ Lab3.py
@@ -0,0 +1,13 @@
+def extract_values_from_dict(input_dict):
+    # Используем генераторное выражение для создания списка значений ключей словаря
+    # Мы просто перебираем значения входного словаря и добавляем их в список
+    values_list = [value for value in input_dict.values()]
+
+    # Возвращаем список значений
+    return values_list
+
+
+# Пример использования функции:
+my_dict = {'a': 1, 'b': 2, 'c': 3}
+result = extract_values_from_dict(my_dict)
+print(result)  # Выведет: [1, 2, 3]
